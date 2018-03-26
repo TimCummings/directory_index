868286ddd16053515e644fefd728bfd8f1b3caf9
Tim Cummings <timjc86@gmail.com>
2018-03-25 22:17:57 -0500

    added sort functionality and link

--- directory_index.rb
+++ directory_index.rb
@@ -5,7 +5,14 @@ require 'sinatra/reloader'
 
 get '/:sort?' do
   @file_list = Dir.glob('public/*').map { |path| File.split(path).last }.sort
-  @file_list.reverse! if params['sort'] == 'descending'
+  if params['sort'] == 'descending'
+    @file_list.reverse!
+    @link = '/'
+    @link_text = 'Sort ascending'
+  else
+    @link = '/descending'
+    @link_text = 'Sort descending'
+  end
 
   erb :home
 end
